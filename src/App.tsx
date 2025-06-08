66e5e0f1a569db1b6c86f01af76130630b94257e
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-06-08 14:46:40 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl'>HELLO</h1>
+      <div className='size-8 bg-black'></div>
+    </>
+  )
+}
+
+export default App
